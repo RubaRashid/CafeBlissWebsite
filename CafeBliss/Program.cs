9a981d5a13c464bc86b54590e35527f07c2e4ac7
RubaRashid <rubarashidkhan@gmail.com>
2026-05-25 02:20:03 +0500

    Structure, Layout, Program.cs and Menu Done

--- CafeBliss/Program.cs
+++ CafeBliss/Program.cs
@@ -1,6 +1,24 @@
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run();
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Menu}/{action=Menu}");
 
 app.Run();
